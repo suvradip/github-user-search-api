fbb86f3838b7049332056898940822bdf94f105b
Suvradip Saha <Suvradip@fusioncharts.com>
2020-04-10 18:08:53 +0530

    build dir added

---
+++ build/precache-manifest.16103d8b7a0c5751d2f887b8da676266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6412583b0d383f42cfd7bbdc457cce86",
+    "url": "/github-users/build/index.html"
+  },
+  {
+    "revision": "4564b8be55dc2e09f861",
+    "url": "/github-users/build/static/css/main.0eb2e749.chunk.css"
+  },
+  {
+    "revision": "b6cf7704926e0f94a6a8",
+    "url": "/github-users/build/static/js/2.37a8d2b1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-users/build/static/js/2.37a8d2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4564b8be55dc2e09f861",
+    "url": "/github-users/build/static/js/main.c4153690.chunk.js"
+  },
+  {
+    "revision": "865dde01eaedb6e0e24c",
+    "url": "/github-users/build/static/js/runtime-main.216f87b9.js"
+  },
+  {
+    "revision": "9f905f2fcaa884b60dc3f297f301e575",
+    "url": "/github-users/build/static/media/logo.9f905f2f.svg"
+  }
+]);
